e715087a9ea5762859a97df3e3262451b7074e7b
Swarnalata Rana <swarnalata21@navgurukul.org>
2023-11-04 16:06:36 +0530

    done

--- app/page.js
+++ app/page.js
@@ -1,97 +1,139 @@
 'use client'
 import React, { useState } from "react";
+
 const Form_Component = () => {
   const [inputlist, setInputlist] = useState("");
   const [items, setItems] = useState([]);
   const [tasktime, setTaskTime] = useState("");
-  const [priority,setPriority] =useState("");
+  const [priority, setPriority] = useState("");
 
   const handlerNameEvent = (event) => {
-    setInputlist(event.target.value);
+      setInputlist(event.target.value);
   };
-  
+
   const handlerTiemEvent = (event) => {
-    setTaskTime(event.target.value);
+      setTaskTime(event.target.value);
   };
-  const handlerPriorityEvent=(event)=>{
-    setPriority(event.target.value)
-  }
 
-  const ListOfItems = () => {
-    if (inputlist.trim() === "" || tasktime.trim() === "") {
-      return;
-    }
-    const NewTask = {
-      name: inputlist,
-      taskTime: tasktime,
-      taskpriority: priority
+  const handlerPriorityEvent = (event) => {
+      setPriority(event.target.value);
+  };
+
+  const UserListOfItems = () => {
+      if (inputlist.trim() === "" || tasktime.trim() === "") {
+        return;
+      }
+      const NewTask = {
+          name: inputlist,
+          taskTime: tasktime,
+          taskpriority: priority,
+          done: false,
+      };
+      setItems([...items, NewTask]);
+      setInputlist("");
+      setTaskTime("");
+      setPriority("");
     };
-    setItems([...items, NewTask]);
-    setInputlist("");
-    setTaskTime("");
-    setPriority("")
-  };
 
-  const deleteItems = (id) => {
-    // Use the correct parameter name 'id'
-    console.log("deleted");
-    setItems((olditems) => {
-      return olditems.filter((_, index) => index !== id);
-    });
-  };
+    const toggleDone = (index) => {
+        const updatedItems = [...items];
+        updatedItems[index].done = !updatedItems[index].done;
+        setItems(updatedItems);
+    };
+
+    const deleteItems = (id) => {
+        setItems((olditems) =>
+          olditems.filter((_, index) => index !== id)
+        );
+    };
+
+    const sortItemsByPriority = () => {
+        const sortedItems = [...items].sort((a, b) => a.taskpriority - b.taskpriority);
+        setItems(sortedItems);
+    };
 
   return (
     <>
       <div className="row_1">
-        <div className="row_2">
-          <h1>Todo List</h1>
-          Enter Your Work Name
-          <input
-            type="text"
-            placeholder="Enter Your Todo Item"
-            value={inputlist}
-            onChange={handlerNameEvent}
-          />
-          Enter Your Time
-          <input
-            type="text"
-            placeholder="Enter Your Todo Item"
-            value={tasktime}
-            onChange={handlerTiemEvent}
-          />
-          Enter Your Priority
-          <select value={priority} onChange={handlerPriorityEvent}>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-
-          <button onClick={ListOfItems}>Add</button>
-
-          <ul className="remove_dot">
-            {items.map((item, index) => (
-              <li key={index}>
-                <span>
-                  Task Name : {item.name}
-                </span>
-                <span>
-                  Task Time : {item.taskTime}
-                </span>
-                <span>
-                  Task Priority : {item.taskpriority}
-                </span>
-                <button   >Done</button>
-                <button onClick={() => deleteItems(index)}>Delete</button>
-              </li>
-            ))}
-          </ul>
-        </div>
+          <div className="row_2">
+              <h1 className="todo_list">Todo List</h1>
+              <div className="work_name">
+            Enter Your Work Name:-
+                <input 
+                  className="input_item"
+                  type="text"
+                  placeholder="Enter Your Todo Item"
+                  value={inputlist}
+                  onChange={handlerNameEvent}
+                />
+            </div>
+            <div>
+            Enter Your Work Time:-
+                <input
+                  className="input_time"
+                  type="text"
+                  placeholder="Enter Your Todo Time"
+                  value={tasktime}
+                  onChange={handlerTiemEvent}
+                />
+            </div >
+            <div className="work_priority_div">
+            Enter Your Work Priority:-
+                <select className="work_priority" value={priority} onChange={handlerPriorityEvent}>
+                    <option value="0">0</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+            </div>
+            <div className="add_btn_div">
+                <button className="add_btn" onClick={UserListOfItems}>Add</button>
+            </div >
+            <div className="sort_btn_div">
+                <button className="sort_btn" onClick={sortItemsByPriority}>Sort by Priority</button>
+            </div>
+            {/* <table className="table">
+              <tr className="">
+                <td>Task Name</td>
+                <td>Task Time</td>
+                <td>Task Priority</td>
+                <td>Sort</td>
+                <td>Done/Undone</td>
+                <td>Delete</td>
+              </tr>
+            </table> */}
+            <ul className="remove_dot">
+              {items.map((item, index) => (
+                <li key={index}>
+                  Task Name:-
+                  <span style={{ textDecoration: item.done ? "line-through" : "none" }}>
+                    <td>{item.name}</td>
+                  </span>
+                  Task Time:-
+                  <span style={{ textDecoration: item.done ? "line-through" : "none" }}>
+                  <td>{item.taskTime}</td>
+                  </span>
+                  Task Priority:-
+                  <span style={{ textDecoration: item.done ? "line-through" : "none" }}>
+                  <td>{item.taskpriority}</td>
+                  </span>
+                  {/* <div className="done_btn_div"> */}
+                      <button className="done_btn" onClick={() => toggleDone(index)}>
+                        {item.done ? "Undone" : "Done"}
+                      </button>
+                  {/* </div> */}
+                  {/* <div className="delete_btn_div"> */}
+                      <button className="done_btn" onClick={() => deleteItems(index)}>Delete</button>
+                  {/* </div> */}
+                </li>
+              ))}
+            </ul>
+          </div>
       </div>
     </>
   );
 };
 
 export default Form_Component;
-
